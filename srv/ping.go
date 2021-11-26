4cf381ac2af9e99279c668cd94e44b7a76083ad6
Frank Ittermann <frank.ittermann@yahoo.de>
2021-11-26 15:56:51 +0100

    Dockerize the ping golang service

--- srv/ping.go
+++ srv/ping.go
@@ -1,49 +1,117 @@
 package main
 
 import (
-    "encoding/json"
-    _ "encoding/json"
-    "net/http"
-    _ "strconv"
+	"encoding/json"
+	_ "encoding/json"
+	"net/http"
+	"reflect"
+	_ "strconv"
+	"strings"
 
-    "github.com/fr123k/fred-the-guardian/pkg/utility"
-    "github.com/gorilla/mux"
+	"github.com/fr123k/fred-the-guardian/pkg/utility"
+	"github.com/fr123k/fred-the-guardian/pkg/model"
+	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/mux"
 )
 
-type PongResponse struct {
-    Response string `json:"response"`
+const (
+	INVALID_REQUEST_BODY = "E400"
+	UNAUTHORIZED_REQUEST = "E401"
+)
+
+// Incase of invalid struct it returns the field name from the json tag instead of the struct variable name.
+func jsonTagName(fld reflect.StructField) string {
+	name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+
+	if name == "-" {
+		return ""
+	}
+
+	return name
 }
 
 func ping(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    // params := mux.Vars(r)
-    // total, err := strconv.ParseInt(params["total"], 10, 64)
-    pong := PongResponse{
-        Response: "pong",
-    }
-    json.NewEncoder(w).Encode(pong)
-    return
+	var pingRqt model.PingRequest
+
+	w.Header().Set("Content-Type", "application/json")
+	// params := mux.Vars(r)
+	// total, err := strconv.ParseInt(params["total"], 10, 64)
+
+	err := json.NewDecoder(r.Body).Decode(&pingRqt)
+	defer r.Body.Close()
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(model.ErrorResponse{
+			Code: INVALID_REQUEST_BODY,
+			//TODO expose service internal error message is not good security practice but good for quick development
+			Error:   err.Error(),
+			Message: "Request body malformed.",
+		})
+		return
+	}
+
+	validate := validator.New()
+	validate.RegisterTagNameFunc(jsonTagName)
+
+	err = validate.Struct(pingRqt)
+	if err != nil {
+		validationErrors := err.(validator.ValidationErrors)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(model.ErrorResponse{
+			Code:    INVALID_REQUEST_BODY,
+			Error:   validationErrors.Error(),
+			Message: "Request body malformed.",
+		})
+		return
+	}
+
+	var pong model.PongResponse
+
+	if pingRqt.Request == "ping" {
+		pong = model.PongResponse{
+			Response: "pong",
+		}
+	} else {
+		//TODO not defined in requirements. Returns the sended request string in the response message
+		pong = model.PongResponse{
+			Response: pingRqt.Request,
+		}
+	}
+
+	json.NewEncoder(w).Encode(pong)
+	return
 }
 
 // Middleware function, which will be called for each request
 func Middleware(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        _ = r.Header.Get("Authorization")
-        // will trigger request processing
-        next.ServeHTTP(w, r)
-        // will stop request processing
-        return
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		secKey := r.Header.Get("X-SECRET-KEY")
+
+		if len(secKey) == 0 {
+			w.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(w).Encode(model.ErrorResponse{
+				Code: UNAUTHORIZED_REQUEST,
+				//TODO expose service internal error message is not good security practice but good for quick development
+				Message: "Missing http header 'X-SECRET-KEY'.",
+			})
+			return
+		}
+		// will trigger request processing
+		next.ServeHTTP(w, r)
+		// will stop request processing
+		return
+	})
 }
 
 func main() {
 
-    router := mux.NewRouter()
+	router := mux.NewRouter()
 
-    router.HandleFunc("/ping", ping).
-        Methods("POST")
+	router.HandleFunc("/ping", ping).
+		Methods("POST")
 
-    router.Use(Middleware)
+	router.Use(Middleware)
 
-    http.ListenAndServe(":"+utility.Env("PORT", "8080"), router)
+	http.ListenAndServe(":"+utility.Env("PORT", "8080"), router)
 }
