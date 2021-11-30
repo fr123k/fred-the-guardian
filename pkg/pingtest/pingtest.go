9864781f603dbaf7904d6d4b50a65158f0380119
Frank Ittermann <frank.ittermann@yahoo.de>
2021-11-30 12:17:11 +0100

    test(rate policy): test to ensure rate policies
    Add test for the two rate limit policies of
    * no more then 2 requests per seconds (TestPingWithGlobalRateLimit)
    * no more then 10 requests per minutes for each secret key
      (TestPingWithBucketRateLimit)
    .

--- pkg/pingtest/pingtest.go
+++ pkg/pingtest/pingtest.go
@@ -1,4 +1,4 @@
-package pingtest
+package HttpTest
 
 import (
     "encoding/json"
@@ -11,34 +11,77 @@ import (
 
     "github.com/fr123k/fred-the-guardian/pkg/model"
     "github.com/gorilla/mux"
+    "github.com/mcuadros/go-defaults"
     "github.com/stretchr/testify/assert"
 )
 
-type PingTestExpect struct {
+type Test interface {
+    toTest() HttpTest
+}
+
+type HttpTestExpect struct {
     Code int
     Body *string
+}
+
+type HttpTest struct {
+    Body   *string
+    Secret *string
+    Method string
+    Path   string
+    Expect HttpTestExpect
+}
+
+type StatusTest struct {
+    Body   *string
+    Secret *string
+    Method string `default:"GET"`
+    Path   string `default:"/status"`
+    Expect HttpTestExpect
 }
 
 type PingTest struct {
     Body   *string
     Secret *string
-    Method string
-    Path   string
-    Expect PingTestExpect
+    Method string `default:"POST"`
+    Path   string `default:"/ping"`
+    Expect HttpTestExpect
+}
+
+func (p *PingTest) toTest() HttpTest {
+    defaults.SetDefaults(p)
+    return HttpTest{
+        Body: p.Body,
+        Secret: p.Secret,
+        Method: p.Method,
+        Path: p.Path,
+        Expect: p.Expect,
+    }
+}
+
+func (s *StatusTest) toTest() HttpTest {
+    defaults.SetDefaults(s)
+    return HttpTest{
+        Body: s.Body,
+        Secret: s.Secret,
+        Method: s.Method,
+        Path: s.Path,
+        Expect: s.Expect,
+    }
 }
 
 func StringPtr(s string) *string {
     return &s
 }
 
-func (tp PingTest) Body_() io.Reader {
+func (tp HttpTest) Body_() io.Reader {
     if tp.Body == nil {
         return nil
     }
     return strings.NewReader(*tp.Body)
 }
 
-func (tp PingTest) Secret_() string {
+func (tp HttpTest) Secret_() string {
     if tp.Secret != nil {
         return *tp.Secret
     }
@@ -55,9 +98,30 @@ func ExpectMissingProperPayload() string {
     return fmt.Sprintf("%s\n", string(str))
 }
 
+func ExpectInvalidRequest(err string) string {
+    str, _ := json.Marshal(model.InValidRequest(err))
+    return fmt.Sprintf("%s\n", string(str))
+}
+
+func ExpectRateLimit(maxCnt uint, wait int64) string {
+    str, _ := json.Marshal(model.TooManyRequests(maxCnt, wait))
+    return fmt.Sprintf("%s\n", string(str))
+}
+
 func PingPayloadRequest(request string) string {
     str, _ := json.Marshal(model.PingRequest{
         Request: request,
+    })
+    return fmt.Sprintf("%s\n", string(str))
+}
+
+type MalformedRequest struct {
+    Unknown string `json:"unknown"`
+}
+
+func MalformedBodyRequest(request string) string {
+    str, _ := json.Marshal(MalformedRequest{
+        Unknown: request,
     })
     return fmt.Sprintf("%s\n", string(str))
 }
@@ -69,7 +133,32 @@ func PingPayloadResponse(response string) string {
     return fmt.Sprintf("%s\n", string(str))
 }
 
-func RunTests(tests map[string]PingTest, rt *mux.Router, t *testing.T) {
+type toHttpTestFunc func(map[string]HttpTest) 
+
+//TODO check if this boilerplate code can be reduced
+func toHttpTest(size uint, f toHttpTestFunc) map[string]HttpTest {
+    httpTests := make(map[string]HttpTest, size)
+    f(httpTests)
+    return httpTests
+}
+
+func PingToHttpTests(tests map[string]PingTest) map[string]HttpTest {
+    return toHttpTest(uint(len(tests)), func(httpTests map[string]HttpTest) {
+        for name, tc := range tests {
+            httpTests[name] = tc.toTest()
+        }
+    })
+}
+
+func StatusToHttpTests(tests map[string]StatusTest) map[string]HttpTest {
+    return toHttpTest(uint(len(tests)), func(httpTests map[string]HttpTest) {
+        for name, tc := range tests {
+            httpTests[name] = tc.toTest()
+        }
+    })
+}
+
+func RunHttpTests(tests map[string]HttpTest, rt *mux.Router, t *testing.T) {
     for name, tc := range tests {
         t.Run(name, func(t *testing.T) {
             r, _ := http.NewRequest(tc.Method, tc.Path, tc.Body_())
--- srv/ping_test.go
+++ srv/ping_test.go
@@ -1,87 +1,150 @@
 package main
 
 import (
-    "net/http"
-    "testing"
+	"fmt"
+	"net/http"
+	"testing"
 
-    . "github.com/fr123k/fred-the-guardian/pkg/pingtest"
+	. "github.com/fr123k/fred-the-guardian/pkg/pingtest"
 )
 
 func TestStatus(t *testing.T) {
-    tests := map[string]PingTest{
+    tests := map[string]StatusTest{
         "missing secret key": {
             Body:   nil,
-            Path:   "/status",
-            Method: "GET",
-            Expect: PingTestExpect{
+            Expect: HttpTestExpect{
                 Code: http.StatusUnauthorized,
                 Body: StringPtr(ExpectMissingHttpHeader()),
             },
         },
         "secret key": {
             Body:   nil,
-            Path:   "/status",
-            Method: "GET",
             Secret: StringPtr("secret"),
-            Expect: PingTestExpect{
+            Expect: HttpTestExpect{
                 Code: http.StatusOK,
                 Body: StringPtr(""),
             },
         },
     }
 
-    RunTests(tests, startRouter(), t)
+    RunHttpTests(StatusToHttpTests(tests), startRouter(), t)
 }
 
-func TestPingEdgeCase(t *testing.T) {
+func TestPing(t *testing.T) {
     tests := map[string]PingTest{
         "missing secret key": {
-            Body:   nil,
-            Path:   "/ping",
-            Method: "POST",
-            Expect: PingTestExpect{
+            Body: nil,
+            Expect: HttpTestExpect{
                 Code: http.StatusUnauthorized,
                 Body: StringPtr(ExpectMissingHttpHeader()),
             },
         },
         "empty payload": {
             Body:   nil,
-            Path:   "/ping",
-            Method: "POST",
             Secret: StringPtr("secret"),
-            Expect: PingTestExpect{
+            Expect: HttpTestExpect{
                 Code: http.StatusBadRequest,
                 Body: StringPtr(ExpectMissingProperPayload()),
             },
         },
-    }
-
-    RunTests(tests, startRouter(), t)
-}
-
-func TestPing(t *testing.T) {
-    tests := map[string]PingTest{
+        "non json payload": {
+            Body:   StringPtr("thats not a json payload"),
+            Secret: StringPtr("secret"),
+            Expect: HttpTestExpect{
+                Code: http.StatusBadRequest,
+                Body: StringPtr(ExpectInvalidRequest("invalid character 'h' in literal true (expecting 'r')")),
+            },
+        },
+        "malformed payload": {
+            Body:   StringPtr(MalformedBodyRequest("malformed body")),
+            Secret: StringPtr("secret"),
+            Expect: HttpTestExpect{
+                Code: http.StatusBadRequest,
+                Body: StringPtr(ExpectInvalidRequest("Key: 'PingRequest.request' Error:Field validation for 'request' failed on the 'required' tag")),
+            },
+        },
         "ping request": {
             Body:   StringPtr(PingPayloadRequest("ping")),
-            Path:   "/ping",
-            Method: "POST",
             Secret: StringPtr("secret"),
-            Expect: PingTestExpect{
+            Expect: HttpTestExpect{
                 Code: http.StatusOK,
                 Body: StringPtr(PingPayloadResponse("pong")),
             },
         },
         "foobar request": {
             Body:   StringPtr(PingPayloadRequest("foobar")),
-            Path:   "/ping",
-            Method: "POST",
             Secret: StringPtr("secret"),
-            Expect: PingTestExpect{
+            Expect: HttpTestExpect{
                 Code: http.StatusOK,
                 Body: StringPtr(PingPayloadResponse("foobar")),
             },
         },
     }
 
-    RunTests(tests, startRouter(), t)
+    RunHttpTests(PingToHttpTests(tests), startRouter(), t)
+}
+
+func TestPingWithGlobalRateLimit(t *testing.T) {
+    tests := map[string]PingTest{
+        "ping request": {
+            Body:   StringPtr(PingPayloadRequest("ping")),
+            Secret: StringPtr("secret"),
+            Expect: HttpTestExpect{
+                Code: http.StatusOK,
+                Body: StringPtr(PingPayloadResponse("pong")),
+            },
+        },
+        "foobar request": {
+            Body:   StringPtr(PingPayloadRequest("foobar")),
+            Secret: StringPtr("secret"),
+            Expect: HttpTestExpect{
+                Code: http.StatusOK,
+                Body: StringPtr(PingPayloadResponse("foobar")),
+            },
+        },
+        "rate limit exceeded": {
+            Body:   StringPtr(PingPayloadRequest("foobar")),
+            Secret: StringPtr("secret"),
+            Expect: HttpTestExpect{
+                Code: http.StatusTooManyRequests,
+                Body: StringPtr(ExpectRateLimit(2, 1)),
+            },
+        },
+    }
+    router := startRouter()
+    RunHttpTests(PingToHttpTests(tests), enableGlobalRateLimit(router), t)
+}
+
+func multipleRequests(cnt uint) map[string]PingTest {
+    tests := map[string]PingTest{}
+    for i := 0; i < 10; i++ {
+        tests[fmt.Sprintf("ping request %d", i)] = PingTest{
+            Body:   StringPtr(PingPayloadRequest("ping")),
+            Secret: StringPtr("secret"),
+            Expect: HttpTestExpect{
+                Code: http.StatusOK,
+                Body: StringPtr(PingPayloadResponse("pong")),
+            },
+        }
+    }
+    return tests
+}
+
+func TestPingWithBucketRateLimit(t *testing.T) {
+    tests := multipleRequests(10)
+    rateLimitExceeded := map[string]PingTest{
+        "rate limit exceeded": {
+            Body:   StringPtr(PingPayloadRequest("foobar")),
+            Secret: StringPtr("secret"),
+            Expect: HttpTestExpect{
+                Code: http.StatusTooManyRequests,
+                Body: StringPtr(ExpectRateLimit(10, 60)),
+            },
+        },
+    }
+    router := startRouter()
+    router = enableBucketRateLimit(router)
+    RunHttpTests(PingToHttpTests(tests), router, t)
+    RunHttpTests(PingToHttpTests(rateLimitExceeded), router, t)
+
 }
