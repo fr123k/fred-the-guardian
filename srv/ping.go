00f7f4cbe1fd6d2d9effef734b1fd04a7002f0c6
Frank Ittermann <frank.ittermann@yahoo.de>
2021-12-01 14:24:55 +0100

    refactor(ping): Restructure ping srv code
    Move common code out to the pkg folder. This includes the middleware
    handlers as well as the validator related function.

--- srv/ping.go
+++ srv/ping.go
@@ -1,151 +1,83 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
-    "net/http"
-    "reflect"
-    "strings"
-    "time"
+	"encoding/json"
+	"net/http"
+	"time"
 
-    "github.com/fr123k/fred-the-guardian/pkg/counter"
-    "github.com/fr123k/fred-the-guardian/pkg/model"
-    "github.com/fr123k/fred-the-guardian/pkg/utility"
+	"github.com/fr123k/fred-the-guardian/pkg/middleware"
+	"github.com/fr123k/fred-the-guardian/pkg/model"
+	"github.com/fr123k/fred-the-guardian/pkg/utility"
 
-    "github.com/go-playground/validator/v10"
-    "github.com/gorilla/mux"
+	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/mux"
 )
 
-// Incase of invalid struct it returns the field name from the json tag instead of the struct variable name.
-func jsonTagName(fld reflect.StructField) string {
-    name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
-
-    if name == "-" {
-        return ""
-    }
-
-    return name
-}
+type HandlerFunc = func (w http.ResponseWriter, r *http.Request)
 
 func status(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusOK)
 }
 
-func ping(w http.ResponseWriter, r *http.Request) {
-    var pingRqt model.PingRequest
-
-    w.Header().Set("Content-Type", "application/json")
-
-    if r.Body == nil {
-        w.WriteHeader(http.StatusBadRequest)
-        json.NewEncoder(w).Encode(model.INVALID_REQUEST_BODY_EMPTY_PAYLOAD)
-        return
-    }
-
-    err := json.NewDecoder(r.Body).Decode(&pingRqt)
-    defer r.Body.Close()
-
-    if err != nil {
-        w.WriteHeader(http.StatusBadRequest)
-        json.NewEncoder(w).Encode(model.ErrorResponse{
-            Code: model.INVALID_REQUEST_BODY,
-            //TODO expose service internal error message is not good security practice but good for quick development
-            Error:   err.Error(),
-            Message: "Request body malformed.",
-        })
-        return
-    }
-
+func ping() HandlerFunc {
     validate := validator.New()
-    validate.RegisterTagNameFunc(jsonTagName)
+    validate.RegisterTagNameFunc(utility.JsonTagName)
+    return func(w http.ResponseWriter, r *http.Request) {
+        var pingRqt model.PingRequest
 
-    err = validate.Struct(pingRqt)
-    if err != nil {
-        validationErrors := err.(validator.ValidationErrors)
-        w.WriteHeader(http.StatusBadRequest)
-        json.NewEncoder(w).Encode(model.InValidRequest(validationErrors.Error()))
-        return
-    }
+        w.Header().Set("Content-Type", "application/json")
 
-    var pong model.PongResponse
-
-    if pingRqt.Request == "ping" {
-        pong = model.PongResponse{
-            Response: "pong",
-        }
-    } else {
-        //TODO not defined in requirements. Returns the sended request string in the response message
-        pong = model.PongResponse{
-            Response: pingRqt.Request,
-        }
-    }
-
-    json.NewEncoder(w).Encode(pong)
-    return
-}
-
-// Middleware function, which will be called for each request
-func Middleware(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        secKey := r.Header.Get("X-SECRET-KEY")
-
-        if len(secKey) == 0 {
-            w.WriteHeader(http.StatusUnauthorized)
-            json.NewEncoder(w).Encode(model.UNAUTHORIZED_REQUEST_RESPONSE)
-            // will stop request processing
+        if r.Body == nil {
+            w.WriteHeader(http.StatusBadRequest)
+            json.NewEncoder(w).Encode(model.INVALID_REQUEST_BODY_EMPTY_PAYLOAD)
             return
         }
 
-        // will trigger request processing
-        next.ServeHTTP(w, r)
-        return
-    })
-}
+        err := json.NewDecoder(r.Body).Decode(&pingRqt)
+        defer r.Body.Close()
 
-// Middleware function, which will be called for each request
-// TODO add name to identify it in logs and tests
-func GlobalCounterMiddleware(maxCnt uint, duration time.Duration) mux.MiddlewareFunc {
-    counter := counter.NewRateLimit(duration)
-    return func(h http.Handler) http.Handler {
-        return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-            // will trigger request processing
-            rate := counter.Increment()
-            if rate.Count > uint64(maxCnt) {
-                w.WriteHeader(http.StatusTooManyRequests)
-                json.NewEncoder(w).Encode(model.TooManyRequests(maxCnt, rate.NextReset))
-                // will stop request processing
-                return
+        if err != nil {
+            w.WriteHeader(http.StatusBadRequest)
+            json.NewEncoder(w).Encode(model.ErrorResponse{
+                Code: model.INVALID_REQUEST_BODY,
+                //TODO expose service internal error message is not good security practice but good for quick development
+                Error:   err.Error(),
+                Message: "Request body malformed.",
+            })
+            return
+        }
+
+        err = validate.Struct(pingRqt)
+        if err != nil {
+            validationErrors := err.(validator.ValidationErrors)
+            w.WriteHeader(http.StatusBadRequest)
+            json.NewEncoder(w).Encode(model.InValidRequest(validationErrors.Error()))
+            return
+        }
+
+        var pong model.PongResponse
+
+        if pingRqt.Request == "ping" {
+            pong = model.PongResponse{
+                Response: "pong",
             }
-            log.Printf("Global Rate %v", rate)
-            h.ServeHTTP(w, r)
-            return
-        })
-    }
-}
-
-// Middleware function, which will be called for each request
-// TODO add name to identify it in logs and tests
-func BucketCountersMiddleware(key string, maxCnt uint, duration time.Duration) mux.MiddlewareFunc {
-    counter := counter.NewBucket(duration)
-    return func(h http.Handler) http.Handler {
-        return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-            // will trigger request processing
-            secKey := r.Header.Get(key)
-            rate := counter.Increment(secKey)
-            if rate.Count > uint64(maxCnt) {
-                w.WriteHeader(http.StatusTooManyRequests)
-                json.NewEncoder(w).Encode(model.TooManyRequests(maxCnt, rate.NextReset))
-                // will stop request processing
-                return
+        } else {
+            //TODO not defined in requirements. Returns the sended request string in the response message
+            pong = model.PongResponse{
+                Response: pingRqt.Request,
             }
-            log.Printf("Bucket Rate %v", rate)
-            h.ServeHTTP(w, r)
-            return
-        })
+        }
+
+        json.NewEncoder(w).Encode(pong)
+        return
     }
 }
 
 func main() {
+    startPingService()
+}
+
+func startPingService() {
     router := startRouter()
     router = enableGlobalRateLimit(router)
     router = enableBucketRateLimit(router)
@@ -155,22 +87,22 @@ func main() {
 func startRouter() *mux.Router {
     router := mux.NewRouter()
 
-    router.HandleFunc("/ping", ping).
+    router.HandleFunc("/ping", ping()).
         Methods("POST")
     router.HandleFunc("/status", status).
         Methods("GET")
 
-    router.Use(Middleware)
+    router.Use(middleware.AuthenticationMiddleware)
 
     return router
 }
 
 func enableGlobalRateLimit(router *mux.Router) *mux.Router {
-    router.Use(GlobalCounterMiddleware(2, 1 * time.Second))
+    router.Use(middleware.GlobalCounterMiddleware(2, 1 * time.Second))
     return router
 }
 
 func enableBucketRateLimit(router *mux.Router) *mux.Router {
-    router.Use(BucketCountersMiddleware("X-SECRET-KEY", 10, 1 * time.Minute))
+    router.Use(middleware.BucketCountersMiddleware(middleware.HTTP_HEADER_SECRET_KEY, 10, 1 * time.Minute))
     return router
 }
