731d74245cfb67a2f1e7fa29683d6eebfbc1273a
Frank Ittermann <frank.ittermann@yahoo.de>
2021-12-03 11:39:35 +0100

    refactor(json): Move func JsonTagName to json.go

--- pkg/utility/json_test.go
+++ pkg/utility/json_test.go
@@ -1,6 +1,7 @@
 package utility
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,4 +14,24 @@ type JsonExample struct {
 func TestPrettyPrint(t *testing.T) {
 	str := PrettyPrint(JsonExample{Print: "foo"})
 	assert.Equal(t, "{\n\t\"Print\": \"foo\"\n}", str, "")
+}
+
+
+func TestJsonTagName(t *testing.T) {
+	name := JsonTagName(reflect.StructField{
+		Tag: `json:"request" validate:"required"`,
+	})
+	assert.Equal(t, "request", name, "")
+}
+
+func TestJsonTagNameNoJsonTag(t *testing.T) {
+	name := JsonTagName(reflect.StructField{
+		Tag: `validate:"required"`,
+	})
+	assert.Equal(t, "", name, "")
+}
+
+func TestJsonTagNameNoTags(t *testing.T) {
+	name := JsonTagName(reflect.StructField{})
+	assert.Equal(t, "", name, "")
 }
