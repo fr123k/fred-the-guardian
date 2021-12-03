66bed5fb99657fa9ff3d2e9a9d144f14d4b5e48b
Frank Ittermann <frank.ittermann@yahoo.de>
2021-12-03 11:39:35 +0100

    refactor(json): Move func JsonTagName to json.go

--- pkg/utility/json.go
+++ pkg/utility/json.go
@@ -1,8 +1,17 @@
 package utility
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"reflect"
+	"strings"
+)
 
 func PrettyPrint(i interface{}) string {
 	s, _ := json.MarshalIndent(i, "", "\t")
 	return string(s)
+}
+
+// In case of invalid struct it returns the field name from the json tag instead of the struct variable name.
+func JsonTagName(fld reflect.StructField) string {
+	return strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
 }
